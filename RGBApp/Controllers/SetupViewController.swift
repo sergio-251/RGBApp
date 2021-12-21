2e82bdbd9709a02a733d6bcd88f7cedd74cd3aca
EfimovSchool <sergio-251@yandex.ru>
2021-12-21 17:39:12 +0300

    SetupV3

--- RGBApp/Controllers/SetupViewController.swift
+++ RGBApp/Controllers/SetupViewController.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class SetupViewController: UIViewController {
     
     @IBOutlet var viewColor: UIView!
     
---
+++ RGBApp/Controllers/StartViewController.swift
@@ -0,0 +1,19 @@
+//
+//  StartViewController.swift
+//  RGBApp
+//
+//  Created by Sergey Efimov on 21.12.2021.
+//
+
+import UIKit
+
+class StartViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+       
+    }
+    
+
+}
