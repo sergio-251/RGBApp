fb19c2217e57c7cbf4ea844aa58a43e6c73b95fa
EfimovSchool <sergio-251@yandex.ru>
2021-12-22 13:34:42 +0300

    Final

--- RGBApp/Controllers/SetupViewController.swift
+++ RGBApp/Controllers/SetupViewController.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class SetupViewController: UIViewController {
+class SetupViewController: UIViewController, UITextFieldDelegate {
     
     @IBOutlet var viewColor: UIView!
     
@@ -35,6 +35,15 @@ class SetupViewController: UIViewController {
         delegate.setColorView(with: rgbColor)
         dismiss(animated: true)
     }
+    
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        updateView(textField)
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        updateView(textField)
+        return true
+    }
 }
 
 //MARK: Static methods
@@ -52,6 +61,11 @@ extension SetupViewController {
 
 //MARK: Private Methods
 extension SetupViewController {
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        super .touchesBegan(touches, with: event)
+        view.endEditing(true)
+    }
     
     private func setLabelValue(for label: UILabel, with color: Float) {
         label.text = String(format: "%.2f", color)
@@ -83,14 +97,57 @@ extension SetupViewController {
         
     }
     
-    private func updateView(_ sender: UISlider) {
-        switch sender.tag {
-        case 0:
-            rgbColor.redColor = sender.value
-        case 1:
-            rgbColor.greenColor = sender.value
-        default:
-            rgbColor.blueColor = sender.value
+    private func validTextFieldValue(text: String, defaultColor: Float) -> Float {
+        if let number = Float(text) {
+            return number
+        }
+        showAlert(title: "Wrong number format!", message: "Please, repeat number input")
+        return defaultColor
+    }
+    
+    private func showAlert(
+        title: String = "",
+        message: String = "",
+        buttonType: String = "OK"
+    ) {
+        let alert = UIAlertController(
+            title: title,
+            message: message,
+            preferredStyle: .alert)
+        present(alert, animated: true)
+        let okAction = UIAlertAction(title: buttonType, style: .default)
+        alert.addAction(okAction)
+    }
+    
+    private func updateView(_ sender: UIControl) {
+        if let sender = sender as? UISlider {
+            switch sender.tag {
+            case 0:
+                rgbColor.redColor = sender.value
+            case 1:
+                rgbColor.greenColor = sender.value
+            default:
+                rgbColor.blueColor = sender.value
+            }
+        }
+        if let sender = sender as? UITextField {
+            switch sender.tag {
+            case 0:
+                rgbColor.redColor = validTextFieldValue(
+                    text: sender.text ?? String(rgbColor.redColor),
+                    defaultColor: rgbColor.redColor
+                )
+            case 1:
+                rgbColor.greenColor = validTextFieldValue(
+                    text: sender.text ?? String(rgbColor.greenColor),
+                    defaultColor: rgbColor.greenColor
+                )
+            default:
+                rgbColor.blueColor = validTextFieldValue(
+                    text: sender.text ?? String(rgbColor.blueColor),
+                    defaultColor: rgbColor.blueColor
+                )
+            }
         }
         updateUI()
         SetupViewController.setColor(to: viewColor, with: rgbColor)
